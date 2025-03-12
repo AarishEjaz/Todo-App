a0eafd7a45e595608d83af95a4fc2630b2ffc2b1
AarishEjaz <aarishejaz@gmail.com>
2025-03-12 12:54:08 +0530

    added frontend playbook

--- todo-backend/server.js
+++ todo-backend/server.js
@@ -6,6 +6,7 @@ const Todo = require('./models/todoModel')
 const dotenv = require('dotenv').config()
 const connectDB = require("./db")
 const todoRoutes = require("./routes/todoRoutes")
+const path = require("path")
 
 
 const app = express()
@@ -25,5 +26,12 @@ app.get("/", async(req, res) => {
 
 
 connectDB();
+
+app.use(express.static(path.join(__dirname, "../todo-frontend/build")))
+
+app.get("*", (req, res) =>{
+  res.sendFile(path.join(__dirname, "../todo-frontend/build", "index.html"))
+})
+
 
 module.exports = app
